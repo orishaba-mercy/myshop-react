6a2190ccddf3caff115e659b043c6fe55af99a36
orishaba-mercy <orishabamercybanga@gmail.com>
2023-07-14 21:38:34 +0300

    made changes

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,37 +1,21 @@
-// import React from "react";
-// import './style.css'
-// // import { Link } from "react-router-dom";
-// // import Products from "./Products";
+import React from "react";
+import './style.css'
+// import { Link } from "react-router-dom";
+// import Products from "./Products";
 
 
-// const NavBar =()=>{
-//     return(
-//         <div className="nav">
-//             <ul>
-//                 <a href="/login">Login</a>
-//                 <a href="/products">Products</a>
-//                 <a href="">Add Products</a>
+const NavBar =()=>{
+    return(
+        <div className="nav">
+            <ul>
+                <a href="/login">Login</a>
+                <a href="/products">Products</a>
+                <a href="">Add Products</a>
                 
             
-//             </ul>
-//         </div>
-//     )
-// }
-// export default NavBar;
-import React from "react";
-import { Link } from "react-router-dom";
-import './style.css';
-
-const NavBar = () => {
-  return (
-    <div className="nav">
-      <ul>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/add-products">Add Products</Link></li>
-      </ul>
-    </div>
-  );
+            </ul>
+        </div>
+    )
 }
-
 export default NavBar;
+
